2ee279cc9917fd0b0d3d63eaaae1c37519b132e2
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-26 12:09:22 -0300

    added routines route

---
+++ routes/routines.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const routineService = require('../services/routineService');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await routineService.getAll();
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const result = await routineService.get(req.params.id);
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const result = await routineService.insert(req.routine);
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        req.body.client_id = req.params.id;
+        const result = await routineService.update(req.body);
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const result = await routineService.remove(req.params.id);
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+module.exports = router;
