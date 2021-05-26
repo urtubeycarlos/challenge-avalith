131b53fcc3667dbf3c606333153b5a4fecc8a1e5
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-26 12:08:55 -0300

    added routine service

---
+++ services/routineService.js
@@ -0,0 +1,88 @@
+const mongoDB = require('../mongo.db');
+
+async function getAll() {
+    const [db, client] = await mongoDB;
+    return new Promise((resolve, reject) => {
+        db.collection('routines').find({}).toArray((error, result) => {
+            client.close();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+async function get(clientId) {
+    if (!clientId) {
+        throw new Error('clientId must be defined');
+    }
+    const [db, client] = await mongoDB;
+    return new Promise((resolve, reject) => {
+        db.collection('routines').findOne({ client_id: clientId }, (error, result) => {
+            client.close();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+async function insert(routine) {
+    if (!routine.client_id) {
+        throw new Error('client_id must be defined inside routine');
+    }
+    const [db, client] = await mongoDB;
+    return new Promise((resolve, reject) => {
+        db.collection('routines').insert(routine, (error, result) => {
+            client.close();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+async function update(routine) {
+    if (!routine.client_id) {
+        throw new Error('client_id must be defined inside routine');
+    }
+    const [db, client] = await mongoDB;
+    return new Promise((resolve, reject) => {
+        const query = { client_id: routine.client_id };
+        db.collection('routines').updateOne(query, routine, (error, result) => {
+            client.close();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+async function remove(clientId) {
+    if (!clientId) {
+        throw new Error('clientId must be defined');
+    }
+    const [db, client] = await mongoDB;
+    return new Promise((resolve, reject) => {
+        const query = { client_id: clientId };
+        db.collection('routines').deleteOne(query, (error, result) => {
+            client.close();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+module.exports = {
+    getAll,
+    get,
+    insert,
+    update,
+    remove,
+};
