59b9aa5f0ff78f31c79583398481ea2965cb04a3
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-28 12:05:09 -0300

    changed await mongoDB by await mongoDB()

--- services/routineService.js
+++ services/routineService.js
@@ -2,12 +2,11 @@ const mongoDB = require('../mongo.db');
 const { checkID } = require('../utils/checkers');
 
 async function getAll() {
-    const [db, client] = await mongoDB;
+    const [db, client] = await mongoDB();
     return new Promise((resolve, reject) => {
         db.collection('routines').find({}).toArray((error, result) => {
             client.close();
             if (error) {
-                console.log(error);
                 return reject(error);
             }
             return resolve(result);
@@ -17,7 +16,7 @@ async function getAll() {
 
 async function get(clientId) {
     checkID(clientId);
-    const [db, client] = await mongoDB;
+    const [db, client] = await mongoDB();
     return new Promise((resolve, reject) => {
         db.collection('routines').findOne({ client_id: clientId }, (error, result) => {
             client.close();
@@ -31,7 +30,7 @@ async function get(clientId) {
 
 async function insert(routine) {
     checkID(routine.client_id);
-    const [db, client] = await mongoDB;
+    const [db, client] = await mongoDB();
     return new Promise((resolve, reject) => {
         db.collection('routines').insert(routine, (error, result) => {
             client.close();
@@ -45,7 +44,7 @@ async function insert(routine) {
 
 async function update(routine) {
     checkID(routine.client_id);
-    const [db, client] = await mongoDB;
+    const [db, client] = await mongoDB();
     return new Promise((resolve, reject) => {
         const query = { client_id: routine.client_id };
         db.collection('routines').updateOne(query, routine, (error, result) => {
@@ -60,7 +59,7 @@ async function update(routine) {
 
 async function remove(clientId) {
     checkID(clientId);
-    const [db, client] = await mongoDB;
+    const [db, client] = await mongoDB();
     return new Promise((resolve, reject) => {
         const query = { client_id: clientId };
         db.collection('routines').deleteOne(query, (error, result) => {
