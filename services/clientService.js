4feaaa4b29f082634a0c7b7499331e8e32994fa8
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-05-31 17:18:48 -0300

    added more methods for clientService

--- services/clientService.js
+++ services/clientService.js
@@ -2,6 +2,66 @@ const mySQLDB = require('../mysql.db');
 const { checkParams, checkID } = require('../utils/checkers');
 const { checkValidDateTime, formatDateTimeToMySQL, checkValidDay } = require('../utils/datetime');
 
+function getClient(clientId) {
+    checkID(clientId);
+    return new Promise((resolve, reject) => {
+        const query = 'select id, name, surname, email, password, role from user where id = ? and role = "client" and active <> 0';
+        const values = [clientId];
+        const db = mySQLDB();
+        db.query(query, values, (error, result) => {
+            db.end();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+function getClients() {
+    return new Promise((resolve, reject) => {
+        const query = 'select id, name, surname, email, password, role from user where role = "client" and active <> 0';
+        const db = mySQLDB();
+        db.query(query, (error, result) => {
+            db.end();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+function getVisits() {
+    return new Promise((resolve, reject) => {
+        const query = 'select clientId, visit_day, visit_date from client_visit';
+        const db = mySQLDB();
+        db.query(query, (error, result) => {
+            db.end();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+function getClientVisits(clientId) {
+    checkID(clientId);
+    return new Promise((resolve, reject) => {
+        const query = 'select clientId, visit_day, visit_date from client_visit where clientId = ?';
+        const values = [clientId];
+        const db = mySQLDB();
+        db.query(query, values, (error, result) => {
+            db.end();
+            if (error) {
+                return reject(error);
+            }
+            return resolve(result);
+        });
+    });
+}
+
 function addVisit({ clientId, day, dateTime }) {
     checkID(clientId);
     checkParams(day, dateTime);
@@ -12,6 +72,7 @@ function addVisit({ clientId, day, dateTime }) {
         const query = 'insert into client_visit (clientId, visit_day, visit_datetime) values (?, ?, ?)';
         const values = [clientId, day, formatDateTimeToMySQL(dateTime)];
         db.query(query, values, (error, result) => {
+            db.end();
             if (error) {
                 return reject(error);
             }
@@ -21,5 +82,9 @@ function addVisit({ clientId, day, dateTime }) {
 }
 
 module.exports = {
+    getClient,
+    getClients,
+    getVisits,
+    getClientVisits,
     addVisit,
 };
