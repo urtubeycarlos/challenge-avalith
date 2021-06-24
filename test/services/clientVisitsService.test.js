d1257e61a348a97284d0ec60a0fde6cc8a1f51c6
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-06-24 16:54:46 -0300

    modified tests with new service

--- test/services/clientVisitsService.test.js
+++ test/services/clientVisitsService.test.js
@@ -2,7 +2,7 @@ const { describe, it } = require('mocha');
 const { beforeEach, afterEach } = require('mocha');
 const { assert } = require('chai');
 const userService = require('../../services/userService');
-const clientService = require('../../services/clientService');
+const clientVisitsService = require('../../services/clientVisitsService');
 
 describe('Testing client service', () => {
     const fakeClients = [
@@ -31,13 +31,13 @@ describe('Testing client service', () => {
             const client = fakeClients[i];
             await userService.insert(client);
         }
-        const clients = await clientService.getAll();
+        const clients = await userService.getAll('client');
         for (let i = 0; i < clients.length; i += 1) {
             const client = clients[i];
             for (let j = 0; j < fakeVisits.length; j += 1) {
                 const visit = fakeVisits[j];
                 visit.clientId = client.id;
-                await clientService.addVisit(visit);
+                await clientVisitsService.addVisit(visit);
             }
         }
     });
@@ -47,121 +47,63 @@ describe('Testing client service', () => {
             const client = fakeClients[i];
             await userService.remove(client);
         }
-        const visits = await clientService.getAllVisits();
+        const visits = await clientVisitsService.getAllVisits();
         for (let i = 0; i < visits.length; i += 1) {
             const visit = visits[i];
-            clientService.removeVisit(visit);
+            clientVisitsService.removeVisit(visit);
         }
     });
 
     describe('main methods', () => {
-        it('getAll', async () => {
-            const result = await clientService.getAll();
-            assert.strictEqual(result.length, 1);
-        });
-
-        it('get', async () => {
-            const allClients = await clientService.getAll();
-            const client = allClients[allClients.length - 1];
-            const result = await clientService.get(client);
-            assert.deepEqual(result, client);
-        });
-
         it('getAllVisits', async () => {
-            const allVisits = await clientService.getAllVisits();
+            const allVisits = await clientVisitsService.getAllVisits();
             assert.strictEqual(allVisits.length, 2);
         });
 
         it('getClientVisits', async () => {
-            const allClients = await clientService.getAll();
+            const allClients = await userService.getAll('client');
             const client = allClients[allClients.length - 1];
-            const result = await clientService.getClientVisits(client.id);
+            const result = await clientVisitsService.getClientVisits(client.id);
             assert.strictEqual(result.length, 2);
         });
 
         it('addVisit', async () => {
-            const allClients = await clientService.getAll();
+            const allClients = await userService.getAll('client');
             const client = allClients[allClients.length - 1];
             const newVisit = {
                 clientId: client.id,
                 visitDay: 4,
                 visitDateTime: '08-06-2021 15:30:00',
             };
-            await clientService.addVisit(newVisit);
-            const result = await clientService.getAllVisits();
+            await clientVisitsService.addVisit(newVisit);
+            const result = await clientVisitsService.getAllVisits();
             assert.strictEqual(result.length, 3);
         });
 
         it('removeVisit', async () => {
             const visit = fakeVisits[0];
-            const allClients = await clientService.getAll();
+            const allClients = await userService.getAll('client');
             const client = allClients[allClients.length - 1];
             visit.clientId = client.id;
-            await clientService.removeVisit(visit);
-            const result = await clientService.getAllVisits();
+            await clientVisitsService.removeVisit(visit);
+            const result = await clientVisitsService.getAllVisits();
             assert.strictEqual(result.length, 1);
         });
     });
 
     describe('border cases', () => {
-        describe('getAll', () => {
-            it('empty result', async () => {
-                for (let i = 0; i < fakeClients.length; i += 1) {
-                    const client = fakeClients[i];
-                    await userService.remove(client);
-                }
-                const result = await clientService.getAll();
-                assert.strictEqual(result.length, 0);
-            });
-        });
-
-        describe('get', () => {
-            it('null or undefined param', async () => {
-                try {
-                    await clientService.get(null);
-                } catch (error) {
-                    assert.isTrue(error instanceof TypeError);
-                }
-            });
-
-            it('undefined or null values', async () => {
-                try {
-                    await clientService.get({ email: null, password: undefined });
-                } catch (error) {
-                    assert.strictEqual(error.code, 'ER_NOT_PARAM');
-                }
-            });
-
-            it('empty param', async () => {
-                try {
-                    await clientService.get({});
-                } catch (error) {
-                    assert.strictEqual(error.code, 'ER_NOT_PARAM');
-                }
-            });
-
-            it('client not exists', async () => {
-                const inexistentClient = {
-                    email: 'erik@coldmail.com',
-                    password: '5678',
-                };
-                const result = await clientService.get(inexistentClient);
-                assert.deepEqual(result, {});
-            });
-        });
-
         describe('getAllVisits', () => {
             it('empty result', async () => {
-                const clients = await clientService.getAll();
+                const clients = await userService.getAll('client');
                 for (let i = 0; i < clients.length; i += 1) {
                     const client = clients[i];
                     for (let j = 0; j < fakeVisits.length; j += 1) {
                         const visit = fakeVisits[j];
                         visit.clientId = client.id;
-                        await clientService.removeVisit(visit);
+                        await clientVisitsService.removeVisit(visit);
                     }
                 }
-                const result = await clientService.getAllVisits();
+                const result = await clientVisitsService.getAllVisits();
                 assert.strictEqual(result.length, 0);
             });
         });
@@ -169,7 +111,7 @@ describe('Testing client service', () => {
         describe('getClientVisits', () => {
             it('null or undefined id', async () => {
                 try {
-                    await clientService.getClientVisits(null);
+                    await clientVisitsService.getClientVisits(null);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_NOT_ID');
                 }
@@ -177,16 +119,16 @@ describe('Testing client service', () => {
 
             it('not integer id', async () => {
                 try {
-                    await clientService.getClientVisits('something');
+                    await clientVisitsService.getClientVisits('something');
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_ID_NOT_INT');
                 }
             });
 
             it('empty result and not existing client', async () => {
-                const clients = await clientService.getAll();
+                const clients = await userService.getAll('client');
                 const client = clients[clients.length - 1];
-                const result = await clientService.getClientVisits(client.id + 1);
+                const result = await clientVisitsService.getClientVisits(client.id + 1);
                 assert.strictEqual(result.length, 0);
             });
         });
@@ -194,7 +136,7 @@ describe('Testing client service', () => {
         describe('addVisit', () => {
             it('null or undefined param', async () => {
                 try {
-                    await clientService.addVisit(null);
+                    await clientVisitsService.addVisit(null);
                 } catch (error) {
                     assert.isTrue(error instanceof TypeError);
                 }
@@ -207,7 +149,7 @@ describe('Testing client service', () => {
                         visitDay: null,
                         visitDateTime: null,
                     };
-                    await clientService.addVisit(badVisit);
+                    await clientVisitsService.addVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_NOT_PARAM');
                 }
@@ -215,15 +157,15 @@ describe('Testing client service', () => {
 
             it('empty param', async () => {
                 try {
-                    await clientService.get({});
+                    await clientVisitsService.addVisit({});
                 } catch (error) {
-                    assert.strictEqual(error.code, 'ER_NOT_PARAM');
+                    assert.strictEqual(error.code, 'ER_NOT_ID');
                 }
             });
 
             it('null or undefined id', async () => {
                 try {
-                    await clientService.addVisit({ clientId: null });
+                    await clientVisitsService.addVisit({ clientId: null });
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_NOT_ID');
                 }
@@ -231,7 +173,7 @@ describe('Testing client service', () => {
 
             it('id not integer', async () => {
                 try {
-                    await clientService.addVisit({ clientId: 'something' });
+                    await clientVisitsService.addVisit({ clientId: 'something' });
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_ID_NOT_INT');
                 }
@@ -244,7 +186,7 @@ describe('Testing client service', () => {
                         visitDay: 9,
                         visitDateTime: '08-06-2021 15:30:00',
                     };
-                    await clientService.addVisit(badVisit);
+                    await clientVisitsService.addVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_BAD_DAY');
                 }
@@ -257,7 +199,7 @@ describe('Testing client service', () => {
                         visitDay: 6,
                         visitDateTime: '08-77-21 156:30:00',
                     };
-                    clientService.addVisit(badVisit);
+                    clientVisitsService.addVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_BAD_DATETIME');
                 }
@@ -268,7 +210,7 @@ describe('Testing client service', () => {
                         visitDay: 6,
                         visitDateTime: '08-06-2021 15-30-00',
                     };
-                    clientService.addVisit(badVisit);
+                    clientVisitsService.addVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_BAD_DATETIME');
                 }
@@ -278,7 +220,7 @@ describe('Testing client service', () => {
         describe('removeVisit', () => {
             it('null or undefined param', async () => {
                 try {
-                    await clientService.removeVisit(null);
+                    await clientVisitsService.removeVisit(null);
                 } catch (error) {
                     assert.isTrue(error instanceof TypeError);
                 }
@@ -291,7 +233,7 @@ describe('Testing client service', () => {
                         visitDay: null,
                         visitDateTime: null,
                     };
-                    await clientService.removeVisit(badVisit);
+                    await clientVisitsService.removeVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_NOT_PARAM');
                 }
@@ -299,7 +241,7 @@ describe('Testing client service', () => {
 
             it('empty param', async () => {
                 try {
-                    await clientService.removeVisit({});
+                    await clientVisitsService.removeVisit({});
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_NOT_ID');
                 }
@@ -307,7 +249,7 @@ describe('Testing client service', () => {
 
             it('null or undefined id', async () => {
                 try {
-                    await clientService.removeVisit({ clientId: null });
+                    await clientVisitsService.removeVisit({ clientId: null });
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_NOT_ID');
                 }
@@ -315,7 +257,7 @@ describe('Testing client service', () => {
 
             it('id not integer', async () => {
                 try {
-                    await clientService.removeVisit({ clientId: 'something' });
+                    await clientVisitsService.removeVisit({ clientId: 'something' });
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_ID_NOT_INT');
                 }
@@ -328,7 +270,7 @@ describe('Testing client service', () => {
                         visitDay: 9,
                         visitDateTime: '08-06-2021 15:30:00',
                     };
-                    await clientService.removeVisit(badVisit);
+                    await clientVisitsService.removeVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_BAD_DAY');
                 }
@@ -341,7 +283,7 @@ describe('Testing client service', () => {
                         visitDay: 6,
                         visitDateTime: '088-06-2021 155:30:00',
                     };
-                    clientService.removeVisit(badVisit);
+                    clientVisitsService.removeVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_BAD_DATETIME');
                 }
@@ -352,21 +294,21 @@ describe('Testing client service', () => {
                         visitDay: 6,
                         visitDateTime: '08-06-2021 15-30-00',
                     };
-                    clientService.removeVisit(badVisit);
+                    clientVisitsService.removeVisit(badVisit);
                 } catch (error) {
                     assert.strictEqual(error.code, 'ER_BAD_DATETIME');
                 }
             });
 
             it('visit not exists', async () => {
-                const allclients = await clientService.getAll();
+                const allclients = await userService.getAll('client');
                 const client = allclients[allclients.length - 1];
                 const inexistentVisit = {
                     clientId: client.id,
                     visitDay: 3,
                     visitDateTime: '08-06-2021 15:30:00',
                 };
-                const result = await clientService.removeVisit(inexistentVisit);
+                const result = await clientVisitsService.removeVisit(inexistentVisit);
                 assert.strictEqual(result.affectedRows, 0);
             });
         });
